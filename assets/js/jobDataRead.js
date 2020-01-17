36d66ba6609f4d227a35cb557f76faf22c8c2ec3
justbrentvanzant <54194838+justbrentvanzant@users.noreply.github.com>
2020-01-16 18:38:30 -0600

    Update jobDataRead.js

--- assets/js/jobDataRead.js
+++ assets/js/jobDataRead.js
@@ -11,7 +11,7 @@ var fullCount = 0;
 var internCount = 0;
 
 function startSystem(){
-  initOfficers();
+  initSystem();
   initButtons();
 }
 //initializes reading and render of event info
@@ -21,13 +21,23 @@ function initSystem() {
                    simpleSheet: true } )
 }
 
-function initOfficers() {
+function initButtons() {
 	document.getElementById("priority-full").onclick = FilterFullTime;
 	document.getElementById("priority-internship").onclick = FilterInternships;
 	document.getElementById("searchBar").onkeyup = FilterString;
 	document.getElementById("searchButton").onclick = FilterString;
 	fullTimeState = document.getElementById("fullTimeState");
 	internState = document.getElementById("internState");	
+}
+function storeData(data) {
+	currData = jobData;	
+}
+function addEntry(bodyElemnt,dataId) {
+	var newRow = document.createElement("tr"); 
+	var newComp = document.createElement("td"); 
+	newComp.innerHTML=currData[dataId].company;	
+	newRow.appendChild(newApply);
+	bodyElement.appendChild(newRow);
 }
 function FilterString(){
 	var searchTerm = document.getElementById("searchBar").value.toLowerCase();
@@ -87,29 +97,23 @@ function storeData(data){
   currData = jobData;
 }
 
-function addOfficer(bodyElement,dataId) {
-	var newRow = document.createElement("tr"); 
-	var newComp = document.createElement("td"); 
-	newComp.innerHTML=currData[dataId].company;	
-	var newTitle = document.createElement("td"); 
-	newTitle.innerHTML=currData[dataId].jobTitle;
-	var newType = document.createElement("td"); 
-	newType.innerHTML=currData[dataId].jobType.replace(/-/g, ' ').toLowerCase()
-    .split(' ')
-    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-    .join(' ');
-	var newLocation = document.createElement("td");
-	newLocation.innerHTML=currData[dataId].jobLocation;
-	var newApply = document.createElement("td");
-	var applyText = document.createElement("a");
-	applyText.innerHTML = "Apply";
-	applyText.href = currData[dataId].jobDetailsUrl;
-	newApply.appendChild(applyText);
-	newRow.appendChild(newComp);
-	newRow.appendChild(newTitle);
-	newRow.appendChild(newType);
-	newRow.appendChild(newLocation);
-	newRow.appendChild(newApply);
-	bodyElement.appendChild(newRow);
+function renderData(typeName,isAsc,bodyElement){
+	while (bodyElement.firstChild) {
+    	bodyElement.removeChild(bodyElement.firstChild);
+  	}
+  	for (var i = 0; i < currData.length; i++) {
+		if (currData[i].isInternship == "no") {
+			fullCount++;
+			if (typeName != "intern" || typeName == "") {
+				addEntry(bodyElement,i);
+			}
+		}
+		else {
+			internCount++;
+			if (typeName == "intern" || typeName == "") {
+				addEntry(bodyElement,i);
+			}
+		}
+	}
 }
 window.addEventListener('DOMContentLoaded', startSystem);
